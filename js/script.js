6bbb0aa16da25ed1b950907db08d29da8018e320
Vadimshidlov <mat23vadim@mail.ru>
2023-02-03 23:04:56 +0300

    feat: add the first main-block

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ const swiper = new Swiper('.slider-main-block', {
 
   // Navigation arrows
   navigation: {
-    nextEl: '.slider-main-block__arrow.swiper-button-next',
-    prevEl: '.slider-main-block__arrow.swiper-button-prev',
+    nextEl: '.body-main-block__arrow.swiper-button-next',
+    prevEl: '.body-main-block__arrow.swiper-button-prev',
   },
 })
